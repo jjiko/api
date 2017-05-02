d5bea39e6d33847816258aea925d0b22c1fde1f5
Joe Jiko <me@joejiko.com>
2017-05-01 22:00:29 -0400

    updates

--- src/ApiCache.php
+++ src/ApiCache.php
@@ -4,7 +4,21 @@ use Illuminate\Database\Eloquent\Model as Model;
 
 class ApiCache extends Model
 {
-  protected $table = 'api_cache';
-  protected $fillable = ['key', 'data'];
-  public $timestamps = true;
+  protected $table = 'site_cache';
+  protected $fillable = ['category', 'key', 'data'];
+  protected $casts = [
+    'data' => 'json'
+  ];
+
+  public function __construct(array $attributes = [])
+  {
+    parent::__construct($attributes);
+
+    $this->connection = 'j5';
+  }
+
+  public function getDataAttribute($value)
+  {
+    return json_decode($value);
+  }
 }
